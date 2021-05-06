9681cb9fa7676c6d65e970dd69c4bf95bc1cc798
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-05-05 17:10:53 -0700

    feat: initial version

---
+++ script.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+/**
+ * Renames the default branch to main if it is currently set to master
+ *
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ */
+export async function script(octokit, repository) {}
